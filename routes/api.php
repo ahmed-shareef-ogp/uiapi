021fda8b294a0ed2d5546d731457b52debd1ae0c
IT <it@pgoffice.gov.mv>
2026-01-19 14:19:06 +0500

    after adding uiapi package - symlinked to uiapi-package folder

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,18 @@
 <?php
 
-use App\Http\Controllers\GenericApiController;
-use App\Services\ComponentConfigService;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+// Duplicate routes disabled: provided by ogp/uiapi package
+// If you need to re-enable local routes, remove these comments.
+// use App\Http\Controllers\GenericApiController;
+// use App\Services\ComponentConfigService;
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Route;
 
-Route::get('ccs/{model}', function (Request $request, string $model) {
-    return app(ComponentConfigService::class)->index($request, $model);
-});
+// Route::get('ccs/{model}', function (Request $request, string $model) {
+//     return app(ComponentConfigService::class)->index($request, $model);
+// });
 
-Route::get('gapi/{model}', [GenericApiController::class, 'index']);
-Route::get('gapi/{model}/{id}', [GenericApiController::class, 'show']);
-Route::post('gapi/{model}', [GenericApiController::class, 'store']);
-Route::put('gapi/{model}/{id}', [GenericApiController::class, 'update']);
-Route::delete('gapi/{model}/{id}', [GenericApiController::class, 'destroy']);
+// Route::get('gapi/{model}', [GenericApiController::class, 'index']);
+// Route::get('gapi/{model}/{id}', [GenericApiController::class, 'show']);
+// Route::post('gapi/{model}', [GenericApiController::class, 'store']);
+// Route::put('gapi/{model}/{id}', [GenericApiController::class, 'update']);
+// Route::delete('gapi/{model}/{id}', [GenericApiController::class, 'destroy']);
